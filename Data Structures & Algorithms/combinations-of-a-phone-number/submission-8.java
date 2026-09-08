f595bcb12f11f421dba7d60c3d3c708afed1e19d
Vedanth J S <83753994+Vedanth-JS@users.noreply.github.com>
2026-09-08 22:12:04 +0530

    Add: combinations-of-a-phone-number - submission-8

---
+++ Data Structures & Algorithms/combinations-of-a-phone-number/submission-8.java
@@ -0,0 +1,22 @@
+class Solution {
+    public List<String> letterCombinations(String digits) {
+        if(digits.isEmpty()){
+            return new ArrayList<>();
+        }
+        List<String> result = new ArrayList<>();
+        result.add("");
+        String[] convert = {
+            "","","abc","def","ghi","jkl","mno","qprs","tuv","wxyz"
+        };
+        for(char digit : digits.toCharArray()){
+            List<String> temp = new ArrayList<>();
+            for(String str: result){
+                for(char c : convert[digit - '0'].toCharArray()){
+                    temp.add(str + c);
+                }
+            }
+            result = temp;
+        }
+        return result;
+    }
+}
